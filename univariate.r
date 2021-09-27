5de2ed2e15bc8dbbddddf53651b9fa07e1716433
ghoziankarami <ghoziankarami@gmail.com>
2021-09-27 16:15:13 +0300

    Your Message

---
+++ univariate.r
@@ -0,0 +1,11 @@
+##Univariate Statistics GEOL 354 
+
+#define the data
+data(mtcars)
+head(mtcars)
+
+#How to generate a histogram
+hist(mtcars$disp)
+
+#How to generate a boxplot
+boxplot(mtcars$disp)
